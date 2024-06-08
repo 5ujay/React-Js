8e43cf3d70d72436d7f5c43e8d94698ee37e8f09
5ujay <sujaypagam777@gmail.com>
2024-06-08 13:55:12 +0530

    react redux , redux toolkit complete lesson with todo app

---
+++ redux/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Addtodo from "./components/Addtodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Redux react-redux redux-toolkit</h1>
+      <Addtodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
