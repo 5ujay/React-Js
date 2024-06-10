e4bec9b5bd82cf35560d403a891b3c72b2b14921
5ujay <sujaypagam777@gmail.com>
2024-06-10 15:26:05 +0530

    redux toolkit configuration store -> store.js , authSlice.js

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import conf from "./conf/conf";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
-    <>
-      <h1>Mega Blog</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO:{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
