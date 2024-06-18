e9e0a4d49f8c9325cd68aec1fb8220275fbb22ad
5ujay <sujaypagam777@gmail.com>
2024-06-18 18:44:58 +0530

    Private route setup

--- authLoginSignup/src/App.jsx
+++ authLoginSignup/src/App.jsx
@@ -5,14 +5,17 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- authLoginSignup/src/components/Header.jsx
+++ authLoginSignup/src/components/Header.jsx
@@ -6,42 +6,52 @@ import { Link, useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
 
-  const logoutClick = () => {
-    console.log("hi");
-  };
+  const user = false;
+
   return (
     <nav className="bg-gray-800 p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <div className="flex-shrink-0">
-          <a href="/" className="text-black text-lg font-bold">
+          <Link href="" className="text-black text-lg font-bold">
             Logo
-          </a>
+          </Link>
         </div>
         <div className="flex">
-          <Link
-            to="/"
-            className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
-          >
-            Home
-          </Link>
-          <Link
-            to="/profile"
-            className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
-          >
-            Profile
-          </Link>
-          <Link
-            to="/login"
-            className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
-          >
-            Login
-          </Link>
-          <Link
-            to="/signup"
-            className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
-          >
-            Signup
-          </Link>
+          {user ? (
+            <>
+              <Link
+                to="/"
+                className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
+              >
+                Home
+              </Link>
+              <Link
+                to="/profile"
+                className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
+              >
+                Profile
+              </Link>
+              <button className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              {" "}
+              <Link
+                to="/login"
+                className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
+              >
+                Login
+              </Link>
+              <Link
+                to="/signup"
+                className="text-black px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
+              >
+                Signup
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
