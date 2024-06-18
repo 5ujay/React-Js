de27fa83b391afef9c5fac16db56a8c1b165de6a
5ujay <sujaypagam777@gmail.com>
2024-06-18 19:36:14 +0530

    update delete

--- authLoginSignup/src/appwrite/appwrite.js
+++
@@ -1,9 +0,0 @@
-import { Client, Account, ID } from "appwrite";
-
-const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("667149ca002238d4b418");
-
-export const account = new Account();
-
-export default client;
